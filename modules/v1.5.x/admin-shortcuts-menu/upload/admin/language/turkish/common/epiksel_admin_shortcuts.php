0cfcd944fc7c43f73da939f5882067d09357ff18
eka77a <eka77a@cb8e3e16-2719-0565-7c8d-a00ed94001fd>
2012-03-16 17:17:34 +0000

    admin-shortcuts-menu eklentisi eklendi.
    git-svn-id: https://epiksel-free.googlecode.com/svn/trunk@13 cb8e3e16-2719-0565-7c8d-a00ed94001fd

---
+++ modules/v1.5.x/admin-shortcuts-menu/upload/admin/language/turkish/common/epiksel_admin_shortcuts.php
@@ -0,0 +1,22 @@
+<?php
+/* Developer: Ekrem KAYA
+Web Page: www.e-piksel.com */
+
+// E-Menu
+$_['text_emenu_add_product']	= 'Ürün Ekle';
+$_['text_emenu_products']		= 'Ürünler';
+$_['text_emenu_add_category']	= 'Kategori Ekle';
+$_['text_emenu_categories']		= 'Kategoriler';
+$_['text_emenu_options']		= 'Seçenekler';
+$_['text_emenu_manufacturer']	= 'Üreticiler';
+$_['text_emenu_information']	= 'Bilgi Sayfaları';
+$_['text_emenu_reviews']		= 'Yorumlar';
+$_['text_emenu_orders']			= 'Siparişler';
+$_['text_emenu_returns']		= 'Ürün İadeleri';
+$_['text_emenu_customers']		= 'Müşteriler';
+$_['text_emenu_coupons']		= 'Kuponlar';
+$_['text_emenu_mail']			= 'Toplu E-Posta';
+$_['text_emenu_settings']		= 'Ayarlar';
+$_['text_emenu_purchased']		= 'Satılan';
+$_['text_emenu_backup_restore']	= 'Yedekle';
+?>
